04d1e9c17d574f090a12b7ddd8cedf1f1d6cf39f
Thushar123 <thushar.soyam94@gmail.com>
2023-02-08 19:48:02 +0530

    git first commit

---
+++ src/container/index.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const BannerSection = () =>{
+    return(
+        <React.Fragment>
+           <div>Banner</div>
+        </React.Fragment>
+      );
+}
+
+export default BannerSection;
